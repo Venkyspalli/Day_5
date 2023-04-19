84eef7e815a4467aefaee419915f4211f0466942
“Venkyspalli” <Spallivenky@gmail.com>
2023-04-20 00:40:51 +0530

    [Venky]:D5_practice

---
+++ Day5_practice/ifelse.sh
@@ -0,0 +1,15 @@
+shanahaz_age=25
+bhargavi_age=24
+sneha_age=23
+
+if [ "$shanahaz_age -ge $bhargavi_age -a $shanahaz -ge $sneha_age" ]
+then
+	echo "shanahaz_age is older girl = $shanahaz_age."
+
+elif [ "$bhargavi_age -age $shanahaz_age -a bhargavi_age -ge $sneha_age" ]
+then
+	echo "$bhargavi_age is older girl."
+
+else 
+	echo "$sneha_age is older girl."
+fi
